d1f795c7ed9e5f11cbbf17793224e4a90d723c2c
edgarmanukyan <edgarmanukyan19@gmail.com>
2023-08-15 23:36:02 +0400

    Added Koin and created apolloModule

---
+++ app/src/main/java/com/simply/birthdayapp/BirthdayApp.kt
@@ -0,0 +1,20 @@
+package com.simply.birthdayapp
+
+import android.app.Application
+import com.simply.birthdayapp.di.apolloModule
+import org.koin.android.BuildConfig
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class BirthdayApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger(if (BuildConfig.DEBUG) Level.ERROR else Level.NONE)
+            androidContext(applicationContext)
+            modules(listOf(apolloModule))
+        }
+    }
+}
--- app/src/main/java/com/simply/birthdayapp/data/ApolloClientImpl.kt
+++ app/src/main/java/com/simply/birthdayapp/data/ApolloClientImpl.kt
@@ -5,24 +5,30 @@ import com.apollographql.apollo3.network.okHttpClient
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
+object ApolloClientImpl {
+
+    private const val BASE_URL = "https://simply-mobile.amiryan.online/graphql"
+
+    fun create(): ApolloClient {
+        return ApolloClient.Builder()
+            .serverUrl(BASE_URL)
+            .okHttpClient(
+                OkHttpClient.Builder()
+                    .addInterceptor(AuthorizationInterceptor())
+                    .build()
+            )
+            .build()
+    }
+}
 
 private class AuthorizationInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request().newBuilder()
             .apply {
-                //Todo: change access_token_value to real tocken
+                //Todo: change access_token_value to real token
                 addHeader("Authorization", "access_token_value")
             }
             .build()
         return chain.proceed(request)
     }
 }
-
-val apolloClient = ApolloClient.Builder()
-    .serverUrl("https://simply-mobile.amiryan.online/graphql")
-    .okHttpClient(
-        OkHttpClient.Builder()
-            .addInterceptor(AuthorizationInterceptor())
-            .build()
-    )
-    .build()
