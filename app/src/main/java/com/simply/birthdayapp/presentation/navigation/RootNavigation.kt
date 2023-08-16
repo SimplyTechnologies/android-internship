88bcf095a561e4a4fa7d34619388138957ecc8ef
Almond-Peach <almondpitch0@gmail.com>
2023-08-16 17:16:18 +0400

    Align package naming with screen naming

--- app/src/main/java/com/simply/birthdayapp/presentation/navigation/RootNavigation.kt
+++ app/src/main/java/com/simply/birthdayapp/presentation/navigation/RootNavigation.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.simply.birthdayapp.presentation.ui.screens.tabs.MainScreen
+import com.simply.birthdayapp.presentation.ui.screens.main.MainScreen
 
 @Composable
 fun RootNavigation() {
--- app/src/main/java/com/simply/birthdayapp/presentation/ui/screens/main/BottomNavBarScaffold.kt
+++ app/src/main/java/com/simply/birthdayapp/presentation/ui/screens/main/BottomNavBarScaffold.kt
@@ -1,4 +1,4 @@
-package com.simply.birthdayapp.presentation.ui.screens.tabs
+package com.simply.birthdayapp.presentation.ui.screens.main
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
