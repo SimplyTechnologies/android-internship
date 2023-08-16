3a61ba165f245ad7cdfd1c0db3f5f6162891241e
Almond-Peach <almondpitch0@gmail.com>
2023-08-16 11:38:17 +0400

    Remove unnecessary BirthdayApp composable

--- app/src/main/java/com/simply/birthdayapp/presentation/MainActivity.kt
+++ app/src/main/java/com/simply/birthdayapp/presentation/MainActivity.kt
@@ -3,13 +3,16 @@ package com.simply.birthdayapp.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.simply.birthdayapp.presentation.ui.BirthdayApp
+import com.simply.birthdayapp.presentation.navigation.TopLevelNavigation
+import com.simply.birthdayapp.presentation.ui.theme.BirthdayAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BirthdayApp()
+            BirthdayAppTheme {
+                TopLevelNavigation()
+            }
         }
     }
 }
