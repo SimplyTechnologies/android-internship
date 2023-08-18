0d6173cd046854cecd49546ada157210bd8ff6dc
Almond-Peach <almondpitch0@gmail.com>
2023-08-18 19:54:58 +0400

    Align shops navigation with the project convention

--- app/src/main/java/com/simply/birthdayapp/presentation/ui/screens/main/shops/ShopsMainScreen.kt
+++ app/src/main/java/com/simply/birthdayapp/presentation/ui/screens/main/shops/ShopsMainScreen.kt
@@ -1,9 +1,22 @@
 package com.simply.birthdayapp.presentation.ui.screens.main.shops
 
 import androidx.compose.runtime.Composable
-import com.simply.birthdayapp.presentation.navigation.ShopsNavigation
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+sealed class ShopsDestination(val route: String) {
+    data object ShopsScreen : ShopsDestination("shops-screen")
+}
 
 @Composable
 fun ShopsMainScreen() {
-    ShopsNavigation()
+    val shopsNavController = rememberNavController()
+
+    NavHost(
+        navController = shopsNavController,
+        startDestination = ShopsDestination.ShopsScreen.route,
+    ) {
+        composable(ShopsDestination.ShopsScreen.route) { ShopsScreen() }
+    }
 }
