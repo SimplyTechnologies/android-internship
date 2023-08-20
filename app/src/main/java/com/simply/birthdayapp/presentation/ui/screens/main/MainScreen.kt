cb274814be9d25b319a7c922fbc12b086cfaf8ba
Almond-Peach <almondpitch0@gmail.com>
2023-08-20 15:19:34 +0400

    Align bottom bar navigation with the project convention

--- app/src/main/java/com/simply/birthdayapp/presentation/ui/screens/main/MainScreen.kt
+++ app/src/main/java/com/simply/birthdayapp/presentation/ui/screens/main/MainScreen.kt
@@ -1,14 +1,30 @@
 package com.simply.birthdayapp.presentation.ui.screens.main
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.simply.birthdayapp.presentation.navigation.BottomNavigation
+
+sealed class BottomBarDestination(val route: String) {
+    data object HomeMainScreen : BottomBarDestination("home-main-screen")
+    data object ShopsMainScreen : BottomBarDestination("shops-main-screen")
+    data object ProfileMainScreen : BottomBarDestination("profile-main-screen")
+}
 
 @Composable
 fun MainScreen() {
-    val mainNavController = rememberNavController()
-    
-    BottomNavBarScaffold(navController = mainNavController) {
-        BottomNavigation(navController = mainNavController)
+    val bottomBarNavController = rememberNavController()
+
+    BottomNavBarScaffold(bottomBarNavController = bottomBarNavController) {
+        NavHost(
+            navController = bottomBarNavController,
+            startDestination = BottomBarDestination.HomeMainScreen.route,
+        ) {
+            composable(BottomBarDestination.HomeMainScreen.route) { }
+
+            composable(BottomBarDestination.ShopsMainScreen.route) { }
+
+            composable(BottomBarDestination.ProfileMainScreen.route) { }
+        }
     }
 }
