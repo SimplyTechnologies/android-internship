8a00505a561fd696c19fba9eb60ee4f7e0eff9df
Almond-Peach <almondpitch0@gmail.com>
2023-08-21 13:45:01 +0400

    Remove custom shop equals and hashcode methods

--- app/src/main/java/com/simply/birthdayapp/presentation/ui/models/Shop.kt
+++ app/src/main/java/com/simply/birthdayapp/presentation/ui/models/Shop.kt
@@ -5,15 +5,4 @@ data class Shop(
     val name: String,
     val image: ByteArray,
     val isFavorite: Boolean?,
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-        if (id != (other as Shop).id) return false
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return id
-    }
-}
+)
