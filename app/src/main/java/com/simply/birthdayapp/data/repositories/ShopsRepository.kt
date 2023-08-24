eaf30a71ef41cf95c22fa3f1c5e21fbbae9f79f8
Almond-Peach <almondpitch0@gmail.com>
2023-08-24 17:39:03 +0400

    Add favouring mutations

--- app/src/main/java/com/simply/birthdayapp/data/repositories/ShopsRepository.kt
+++ app/src/main/java/com/simply/birthdayapp/data/repositories/ShopsRepository.kt
@@ -1,15 +1,19 @@
 package com.simply.birthdayapp.data.repositories
 
 import com.apollographql.apollo3.ApolloClient
+import com.simply.birthdayapp.AddShopToFavouriteMutation
+import com.simply.birthdayapp.RemoveShopFromFavouriteMutation
 import com.simply.birthdayapp.ShopsQuery
 import com.simply.birthdayapp.data.mappers.toShop
-import com.simply.birthdayapp.presentation.ui.models.Shop
+import com.simply.birthdayapp.presentation.models.Shop
 import com.simply.birthdayapp.type.ShopFilter
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 interface ShopsRepository {
     suspend fun getShops(): List<Shop>
+    suspend fun addShopToFavourites(shopId: Int): Int
+    suspend fun removeShopFromFavourites(shopId: Int): Int
 }
 
 class ShopsRepositoryImpl(
@@ -24,4 +28,27 @@ class ShopsRepositoryImpl(
             ?.map(ShopsQuery.Shop::toShop)
             ?: emptyList()
     }
+
+    override suspend fun addShopToFavourites(shopId: Int): Int = withContext(Dispatchers.IO) {
+        apolloClient
+            .mutation(AddShopToFavouriteMutation(shopId))
+            .execute()
+            .data
+            ?.addShopToFavorite
+            ?.shopId
+            ?: throw FailedToAddShopToFavourites()
+    }
+
+    override suspend fun removeShopFromFavourites(shopId: Int): Int = withContext(Dispatchers.IO) {
+        apolloClient
+            .mutation(RemoveShopFromFavouriteMutation(shopId))
+            .execute()
+            .data
+            ?.removeShopFromFavorite
+            ?.shopId
+            ?: throw FailedToRemoveShopFromFavourites()
+    }
 }
+
+class FailedToAddShopToFavourites : RuntimeException()
+class FailedToRemoveShopFromFavourites : RuntimeException()
