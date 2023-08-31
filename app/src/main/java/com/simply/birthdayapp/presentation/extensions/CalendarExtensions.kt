e634f8eb71dcd9350cc791e8d565a29a406c7c41
Mariam Avanesyan <avanesyan.mariam98@gmail.com>
2023-08-31 17:17:37 +0400

    Update add event to calendar duration

--- app/src/main/java/com/simply/birthdayapp/presentation/extensions/CalendarExtensions.kt
+++ app/src/main/java/com/simply/birthdayapp/presentation/extensions/CalendarExtensions.kt
@@ -5,12 +5,27 @@ import android.content.Context
 import android.net.Uri
 import android.provider.CalendarContract
 import com.simply.birthdayapp.R
+import java.util.Calendar
+import java.util.Date
 import java.util.TimeZone
 
 fun Context.addEventToCalendar(date: Long, name: String): Uri? {
+    val calendar: Calendar = Calendar.getInstance()
+    calendar.time = Date(date)
+
+    calendar[Calendar.HOUR_OF_DAY] = 0
+    calendar[Calendar.MINUTE] = 0
+    calendar[Calendar.SECOND] = 0
+    val startTime = calendar.timeInMillis
+
+    calendar[Calendar.HOUR_OF_DAY] = 23
+    calendar[Calendar.MINUTE] = 59
+    calendar[Calendar.SECOND] = 59
+    val endTime = calendar.timeInMillis
+
     val values = ContentValues().apply {
-        put(CalendarContract.Events.DTSTART, date)
-        put(CalendarContract.Events.DTEND, date)
+        put(CalendarContract.Events.DTSTART, startTime)
+        put(CalendarContract.Events.DTEND, endTime)
         put(
             CalendarContract.Events.TITLE,
             getString(
