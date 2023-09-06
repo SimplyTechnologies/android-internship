f3d9671e65200fcc0550b106560c1ede76eb9b31
Mariam Avanesyan <avanesyan.mariam98@gmail.com>
2023-09-06 14:28:24 +0400

    Add event to current year

--- app/src/main/java/com/simply/birthdayapp/presentation/extensions/CalendarExtensions.kt
+++ app/src/main/java/com/simply/birthdayapp/presentation/extensions/CalendarExtensions.kt
@@ -7,6 +7,8 @@ import android.net.Uri
 import android.provider.CalendarContract
 import com.simply.birthdayapp.R
 import com.simply.birthdayapp.presentation.models.CalendarContractItem
+import java.util.Calendar
+import java.util.Date
 import java.util.TimeZone
 
 @SuppressLint("Range")
@@ -38,9 +40,13 @@ fun Context.addEventToCalendar(email: String, date: Long, name: String): Uri? {
 
     val userEmailContract = contractList.find { it.ownerAccount == email }
     val id = userEmailContract?.calendarId ?: if (contractList.isNotEmpty()) contractList[0].calendarId else return null
+    val dateWithCurrentYear = Calendar.getInstance().apply {
+        time = Date(date)
+        set(Calendar.YEAR, Calendar.getInstance()[Calendar.YEAR])
+    }.time.time
     val values = ContentValues().apply {
-        put(CalendarContract.Events.DTSTART, date)
-        put(CalendarContract.Events.DTEND, date)
+        put(CalendarContract.Events.DTSTART, dateWithCurrentYear)
+        put(CalendarContract.Events.DTEND, dateWithCurrentYear)
         put(CalendarContract.Events.ALL_DAY, true)
         put(CalendarContract.Events.RRULE, "FREQ=YEARLY")
         put(
